4b581b64cbb16fbfc375c987a50663c77d8c5991
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-10-24 09:00:13 +0500

    Lecture 2
    Lecture practice.

---
+++ practice.py
@@ -0,0 +1,32 @@
+# mySalary = 100000
+# myTax = 16/100
+# myPayableTax = mySalary*myTax
+# print(mySalary-myPayableTax)
+
+# print(len('Welcome to today\'s lecture'))
+
+# s = 'My name is {a} and my age is {b}'.format(a='Faisal', b=19)
+# # print(s)
+# # s = 'Hassan'
+# print(s)
+
+# my_list = [1,2,3,4]
+# # # my_list[1] = 5
+# # my_list.append(5)
+# # print(my_list)
+# # my_list.pop(0)
+# # print(my_list)
+# # my_list.pop(0)
+# # print(my_list)
+# my_list.reverse()
+# print(my_list)
+
+lst_1=[1,2,3]
+lst_2=[4,5,6]
+lst_3=[7,8,9]
+
+matrix = [lst_1,lst_2,lst_3]
+
+my_matrix = [matrix[1][1:], matrix[2][1:]]
+
+print(my_matrix)
