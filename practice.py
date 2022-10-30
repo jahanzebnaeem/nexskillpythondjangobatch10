0dc93d09a0ebb56a75eac186b77c2b560f9c7ad7
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-10-30 18:01:55 +0500

    Lecture 4
    Sets, conditional statements and loops.

--- practice.py
+++ practice.py
@@ -31,10 +31,77 @@
 #
 # print(my_matrix)
 
-my_dict = {'name': 'Jahanzeb', 'age': '75', 'product': [2, 333, 675]}
-# my_dict['product'][1] -= 123
-# print(my_dict['product'][1])
+# my_dict = {'name': 'Jahanzeb', 'age': '75', 'product': [2, 333, 675]}
+# # my_dict['product'][1] -= 123
+# # print(my_dict['product'][1])
+#
+# print(my_dict.keys())
+# print(my_dict.values())
+# print(my_dict.items())
 
-print(my_dict.keys())
-print(my_dict.values())
-print(my_dict.items())
+# t = (1,2,3,1,2,4,6,1)
+# # t[2] = 'Ali'
+# t.add('Ali')
+# print(t)
+
+# y = set()
+# y.add(1)
+# y.add(2)
+# y.add(1+2)
+# y.add(1)
+# y.add(2)
+# print(y)
+
+# l = [1,1,2,2,3,4,5,6,1,1]
+# se = set(l)
+# print(se)
+
+# if 1==1:
+#     print('Hello')
+# else:
+#     print('world!')
+#     print('Hello again')
+
+# if 1 == 1:
+#     print('first')
+# elif 3 == 3:
+#     print('middle')
+# else:
+#     print('Last')
+
+# seq = [1,2,3,4,5]
+#
+# for item in seq:
+#     print(item+item)
+
+# ages = {"Sam":3,"Frank":4,"Dan":29}
+#
+# for key in ages:
+#     print("This is the key")
+#     print(key)
+#     print("This is the value")
+#     print(ages[key])
+#     print("\n")
+#
+# mypairs = [(1,10),(3,30),(5,50)]
+#
+# for tup1, tup2 in mypairs:
+#     print(tup1)
+#     print(tup2)
+
+# my_list = list(range(1,10))
+# print(my_list)
+
+# for i in range(0, 10, 2):
+#     print(i)
+
+x = [1,2,3,4]
+
+# out = []
+# for item in x:
+#     out.append(item**2)
+#
+# print(out)
+
+out = [item**2 for item in x]
+print(out)
