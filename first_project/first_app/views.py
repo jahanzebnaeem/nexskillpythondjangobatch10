48d8951eaa78d741289bd621fae7e555512a4563
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-11-20 15:13:39 +0500

    Lecture 9
    Templates and static files.

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello class, mubarak ho')
+    # return HttpResponse('Hello class, mubarak ho')
+    my_greeting = {'insert_me':'Hello class'}
+    return render(request, 'first_app/index.html', context=my_greeting)
